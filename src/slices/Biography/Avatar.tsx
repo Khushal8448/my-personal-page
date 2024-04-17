c5001b116b6cdea676e0e6dd527e1187006946a6
Khushal Mali <khushalmali8448@gmail.com>
2024-04-17 18:51:56 +0530

    about

--- src/slices/Biography/Avatar.tsx
+++ src/slices/Biography/Avatar.tsx
@@ -32,10 +32,43 @@ const Avatar = ({ image, className }: AvatarProps) => {
 
       window.onmousemove = (e) => {
         if (!component.current) return;
-      };
-    });
 
-    return () => ctx.revert();
+        const componentRect = (
+          component.current as HTMLElement
+        ).getBoundingClientRect();
+
+        const componentCenterX = componentRect.left + componentRect.width / 2;
+
+        let componentPercent = {
+          x: e.clientX - componentCenterX / componentRect.width / 2,
+        };
+
+        let distFromCenter = 1 - Math.abs(componentPercent.x);
+
+        gsap
+          .timeline({
+            defaults: { duration: 0.5, overwrite: "auto", ease: "power3.out" },
+          })
+          .to(
+            ".avatar",
+            {
+              rotation: gsap.utils.clamp(-2, 2, 5 * componentPercent.x),
+              duration: 0.5,
+            },
+            0
+          )
+          .to(
+            ".highlight",
+            {
+              opacity: distFromCenter - 0.7,
+              x: (-10 + 20) & componentPercent.x,
+              duration: -5,
+            },
+            0
+          );
+      };
+    }, component);
+
   }, []);
 
   return (
