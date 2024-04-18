f442cfff38cc341d82023cfacac1f105f78d734e
Khushal Mali <khushalmali8448@gmail.com>
2024-04-18 11:21:39 +0530

    Creating blog page

--- src/slices/Biography/Avatar.tsx
+++ src/slices/Biography/Avatar.tsx
@@ -1,18 +1,21 @@
 "use client";
 
-import type { ImageField } from "@prismicio/client";
+import { useEffect, useRef } from "react";
+import gsap from "gsap";
+import { ImageField } from "@prismicio/client";
 import { PrismicNextImage } from "@prismicio/next";
 import clsx from "clsx";
-import { gsap } from "gsap";
-import React, { useEffect, useRef } from "react";
+// import usePrefersReducedMotion from "@/hooks/usePrefersReducedMotion";
 
-type AvatarProps = {
+export default function Avatar({
+  image,
+  className,
+}: {
   image: ImageField;
   className?: string;
-};
-
-const Avatar = ({ image, className }: AvatarProps) => {
+}) {
   const component = useRef(null);
+  // const prefersReducedMotion = usePrefersReducedMotion?.();
 
   useEffect(() => {
     let ctx = gsap.context(() => {
@@ -23,27 +26,26 @@ const Avatar = ({ image, className }: AvatarProps) => {
           scale: 1.4,
         },
         {
-          opacity: 1,
           scale: 1,
+          opacity: 1,
           duration: 1.3,
           ease: "power3.inOut",
         }
       );
 
       window.onmousemove = (e) => {
-        if (!component.current) return;
+        if (!component.current) return; // no component, no animation!
 
         const componentRect = (
           component.current as HTMLElement
         ).getBoundingClientRect();
-
         const componentCenterX = componentRect.left + componentRect.width / 2;
 
         let componentPercent = {
-          x: e.clientX - componentCenterX / componentRect.width / 2,
+          x: (e.clientX - componentCenterX) / componentRect.width / 2,
         };
 
-        let distFromCenter = 1 - Math.abs(componentPercent.x);
+        let distFromCenterX = 1 - Math.abs(componentPercent.x);
 
         gsap
           .timeline({
@@ -60,20 +62,24 @@ const Avatar = ({ image, className }: AvatarProps) => {
           .to(
             ".highlight",
             {
-              opacity: distFromCenter - 0.7,
-              x: (-10 + 20) & componentPercent.x,
-              duration: -5,
+              opacity: distFromCenterX - 0.7,
+              x: -10 + 20 * componentPercent.x,
+              duration: 0.5,
             },
             0
           );
       };
     }, component);
 
+    return () => ctx.revert(); // cleanup!
   }, []);
 
   return (
     <div ref={component} className={clsx("relative h-full w-full", className)}>
-      <div className="avatar aspect-square overflow-hidden rounded-3xl border-2 border-slate-700 opacity-0">
+      <div
+        className="avatar aspect-square overflow-hidden rounded-3xl border-2 border-slate-700 opacity-0"
+        style={{ perspective: "500px", perspectiveOrigin: "150% 150%" }}
+      >
         <PrismicNextImage
           field={image}
           className="avatar-image h-full w-full object-fill"
@@ -83,6 +89,4 @@ const Avatar = ({ image, className }: AvatarProps) => {
       </div>
     </div>
   );
-};
-
-export default Avatar;
+}
